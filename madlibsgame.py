e575190bfcaafff80ee583d4dffe8414f6900c55
meetaliverma <2006224@kiit.ac.in>
2023-02-13 09:49:45 +0530

    Add files via upload

---
+++ madlibsgame.py
@@ -0,0 +1,6 @@
+colour = input("Enter a colour:")
+noun = input("Enter plural noun:")
+celebrity = input("Enter your favourite celebrity's name:")
+print("Roses are"+colour)
+print(noun+" are blue")
+print("I love "+ celebrity)
